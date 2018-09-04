b4d3a1cc0ea2a81fc9a0afeba9944dab9dbe2e51
Wajahat Karim <wajahatkarim3@gmail.com>
2018-09-04 23:20:04 +0500

    Update MainActivity.kt

--- app/src/main/java/com/wajahatkarim3/easyfirebase/demo/MainActivity.kt
+++ app/src/main/java/com/wajahatkarim3/easyfirebase/demo/MainActivity.kt
@@ -7,10 +7,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        //asdf
-
-
+        setContentView(R.layout.activity_main);
     }
 }
